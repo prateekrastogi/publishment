4aff672711535810b97247ddffb280981bd2b27f
prateekrastogi <prastogi34@gmail.com>
2019-08-14 16:55:44 +0530

    changed site logo

--- src/website-config.ts
+++ src/website-config.ts
@@ -52,7 +52,7 @@ const config: WebsiteConfig = {
   title: 'Publishment',
   description: 'The Publishing Establishment',
   coverImage: 'img/blog-cover.jpg',
-  logo: 'img/ghost-logo.png',
+  logo: 'img/logo.png',
   lang: 'en',
   siteUrl: 'https://publishment.netlify.com',
   twitter: 'https://twitter.com/technology',
